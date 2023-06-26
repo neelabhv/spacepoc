0e25e5bbcc8787a6b01d34a8cc5aa8916fdcf937
neelabhv <neelabhv@gmail.com>
2023-06-26 16:03:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DisplayData from './DisplayData.js';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayData/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DisplayData.js
@@ -0,0 +1,47 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+const BASE_URL = "https://api.spacexdata.com/v3/launches?limit=100&";
+
+export default function DisplayData(){
+    const [apiData, setApidata] = useState([]);
+    const [filters, setFilters] =  useState({
+        launch_success: null,
+        land_success: null,
+        launch_year: null,
+      });
+      const time = [
+        2006,2007,2008,2009, 2010,2011, 2012,2013,2014,2015,2016,2017,2018,2019,
+        2020,2021,2022,2023,2024
+      ];
+   
+
+    useEffect(()=>{
+        axios.get(BASE_URL)
+        .then((result)=>{
+            setApidata(result.data);
+        }).catch((err)=>{
+            console.error(err);
+        })
+    },[]);
+
+    const renderYear=()=>time.map((yr)=><button value={yr}>{yr}</button>)
+
+    const updateFilter=(filter, value)=>{
+        console.log("updating filters ******** + ", filter + " " + value);
+
+    }
+    return (
+    <div>
+        Hello Display
+        <div>
+            {renderYear()}
+            <button value="true" id="launch_success" 
+            onClick={(e)=>updateFilter("launch_success" , e.target.value)}>TRUE</button>
+            <button value="false" id="launch_success" 
+            onClick={(e)=>updateFilter("launch_success" , e.target.value)}>FALSE</button>
+        </div>
+    </div>
+    );
+}
