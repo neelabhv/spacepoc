9b189f270f7338bae24204351a5c866d667b4f9c
neelabhv <neelabhv@gmail.com>
2023-07-07 17:12:00 +0530

    temporary fix for updating url with queryparams

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import DisplayData from './DisplayData.js';
 function App() {
   return (
     <div className="App">
-      {window.history.pushState("spaceX","/spaceX")}
       <DisplayData/>
     </div>
   );
